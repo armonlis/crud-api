15e572e9404eabac8df81a6246254fbae5446b50
Siarhei Khonski <armonius@mail.ru>
2022-06-18 09:54:25 +0300

    fix: fix handlers

--- src/ts/handlers/getHandler.ts
+++ src/ts/handlers/getHandler.ts
@@ -9,7 +9,7 @@ export default function getHandler(url: Request["url"]): IResponse | null {
     const data = usersStorage.getUsers();
     return { status: 200, statusMes: STATUS_CODES["200"], data, sendRes: true }; 
   }
-  if (/^\/api\/users\/[a-z0-9-]{1,}$/.test(url)) {
+  if (/^\/api\/users\/[a-zA-Z0-9-]{1,}$/.test(url)) {
     const userId = url.slice(11);
     if (!isValidUUID(userId)) { return { status: 400, statusMes: `${STATUS_CODES["400"]} : User ID is invalid.`, sendRes: true }; }
     if (!usersStorage.isExist(userId)) { return { status: 404, statusMes: `${STATUS_CODES["404"]} : User with id = ${userId} is not exist.`, sendRes: true }; }
