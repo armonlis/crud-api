0f854e91a26d3406033c147fc0288cbb7da85b54
Siarhei Khonski <armonius@mail.ru>
2022-06-18 09:47:41 +0300

    fix: fix get, post, put, delete handlers

--- src/ts/handlers/deleteHandler.ts
+++ src/ts/handlers/deleteHandler.ts
@@ -5,7 +5,7 @@ import { validate as isValidUUID } from "uuid";
 
 
 export default function deleteHandler(url: Request["url"]): IResponse | null {
-  if (/\/api\/users\/\w{1,}/.test(url)) {
+  if (/^\/api\/users\/[a-z0-9-]{1,}$/.test(url)) {
     const userId = url.slice(11);
     if (!isValidUUID(userId)) { return { status: 400, statusMes: `${STATUS_CODES["400"]} : User ID is invalid.`, sendRes: true }; }
     if (!usersStorage.isExist(userId)) { return { status: 404, statusMes: `${STATUS_CODES["404"]} : User with id = ${userId} is not exist.`, sendRes: true }; }
--- src/ts/handlers/postHandler.ts
+++ src/ts/handlers/postHandler.ts
@@ -6,7 +6,7 @@ import validateUser from "./validateUser.js";
 
 
 export default function postHandler(url: Request["url"], userData: IAddedUser ): IResponse | null {
-  if (/\/api\/users/.test(url)) {
+  if (/^\/api\/users\/{0,1}$/.test(url)) {
     if (!validateUser(userData)) { return { status: 400, statusMes: `${STATUS_CODES["400"]} : The user's data is invalid.`, sendRes: true } }
     const data = usersStorage.addUser(userData);
     return { status: 201, statusMes: STATUS_CODES["201"], data, sendRes: true };
