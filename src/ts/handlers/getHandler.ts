4e82baf053e8c14d6b5cf7630e5d0a749be5276a
Siarhei Khonski <armonius@mail.ru>
2022-06-18 09:25:53 +0300

    fix: fix getHandler

--- src/ts/handlers/getHandler.ts
+++ src/ts/handlers/getHandler.ts
@@ -5,11 +5,11 @@ import { validate as isValidUUID } from "uuid";
 
 
 export default function getHandler(url: Request["url"]): IResponse | null {
-  if (url === "/api/users") { 
+  if (/^\/api\/users$/.test(url)) { 
     const data = usersStorage.getUsers();
     return { status: 200, statusMes: STATUS_CODES["200"], data, sendRes: true }; 
   }
-  if (/\/api\/users\/\w{1,}/.test(url)) {
+  if (/^\/api\/users\/\w{1,}$/.test(url)) {
     const userId = url.slice(11);
     if (!isValidUUID(userId)) { return { status: 400, statusMes: `${STATUS_CODES["400"]} : User ID is invalid.`, sendRes: true }; }
     if (!usersStorage.isExist(userId)) { return { status: 404, statusMes: `${STATUS_CODES["404"]} : User with id = ${userId} is not exist.`, sendRes: true }; }
